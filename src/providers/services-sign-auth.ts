bbeee24821cad67a2539b1063082c24c6aff42ec
andrews-felipe <ferulip@gmail.com>
2018-09-02 01:52:12 -0300

    Method for signUp user in system and header of method persistence info

--- src/providers/services-sign-auth.ts
+++ src/providers/services-sign-auth.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
 
 
 
@@ -11,7 +10,9 @@ export class SignAuthService {
   constructor(public http: HttpClient, private fireAuth : AngularFireAuth) {
 
   }
-
+  /***
+   * Method for validate user in system.
+   */
   authLoginValidate(user){
     return new Promise((resolve, reject)=>{
       this.fireAuth.auth.signInWithEmailAndPassword(
@@ -24,6 +25,27 @@ export class SignAuthService {
       });
     })
   }
+
+  singUpUser(user){
+    return new Promise((resolve, reject)=>{
+      this.fireAuth.auth.createUserWithEmailAndPassword(
+        user.email,
+        user.password
+      ).then(res=>{
+        resolve();
+        this.registerInfoUser(user)
+      }, error =>{
+        reject(error)
+      }
+    )
+    })
+  }
+
+  registerInfoUser(user){
+
+  }
+
+  
 
 
   
