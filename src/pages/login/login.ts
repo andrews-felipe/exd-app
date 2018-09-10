7cb3aac436c7634180f992fe50231f2c3178c802
Mateus Lourenço <mateus.lourenco.95@gmail.com>
2018-09-10 19:11:21 -0300

    subindo as alteracoes de bitao

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the LoginPage page.
@@ -15,11 +16,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  user: User = new User();
+    @ViewChild('form') form: NgForm;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+    constructor (public navCtrl: NavController, private  toastCtrl: ToastController, private authService: AuthService) {
 
+    }
+
+    createAccount() {
+        this.navCtrl.push(SignupPage);
+    }
+
+    resetPassword() {
+        this.navCtrl.push(ResetpasswordPage);
+    }
+
+    signIn() {
+
+        if(this.form.form.valid) {
+                
+            this.authService.signIn(this.user)
+            .then(() => {
+                this.navCtrl.setRoot(HomePage);
+            })
+            .catch((error: any) => {
+
+                let toast = this.toastCtrl.create({ duration: 3000, position: 'bottom'});
+
+                if(error.code == 'auth/invalid-email') {
+                    toast.setMessage('O e-mail digitado não é valido.');
+                } 
+                else if(error.code == 'auth/user-disabled') {
+                    toast.setMessage('O usuário está desativado.');
+                } 
+                else if(error.code == 'auth/user-not-found') {
+                    toast.setMessage('O usuário não foi encontrado.');
+                } 
+                else if(error.code == 'auth/wrong-password') {
+                    toast.setMessage('A senha digitada não é valida.');
+                }
+                toast.present();
+            });
+        }
+        
+    }
 }
