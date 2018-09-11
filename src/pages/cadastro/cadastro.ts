d0902d135ad225c6d0c9eb07f9f80039f66d2bec
ayrtoncarlos <bitao.metal@gmail.com>
2018-09-11 00:55:05 -0300

    subindo metodos de validação cadastro de usuário

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+import { User } from '../../providers/auth/user';
+import { AuthService } from '../../providers/auth/auth-service';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the CadastroPage page.
@@ -15,11 +19,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CadastroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user: User = new User();
+  @ViewChild('form') form: NgForm;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toastCtrl: ToastController, private authService: AuthService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CadastroPage');
   }
 
+  createAccount() {
+        
+    if(this.form.form.valid) {
+
+        let toast = this.toastCtrl.create({ duration: 3000, position: 'bottom'});
+
+        this.authService.createUser(this.user)
+            .then((user: any) => {
+                user.sendEmailVerification();
+
+                toast.setMessage('Usuário criado com sucesso.');
+                toast.present();
+
+                this.navCtrl.setRoot(HomePage);
+            })
+            .catch((error: any) => {
+
+                if(error.code == 'auth/email-already-in-use') {
+                    toast.setMessage('O e-mail digitado já está em uso.');
+                }
+                else if(error.code == 'auth/invalid-email') {
+                    toast.setMessage('O e-mail digitado não é valido.');
+                }
+                else if(error.code == 'auth/operation-not-allowed') {
+                    toast.setMessage('Não está habilitado criar usuários.');
+                }
+                else if(error.code == 'auth/weak-password') {
+                    toast.setMessage('A senha digitada é muito fraca.');
+                }
+                toast.present();
+            });
+    }
+    
+  }
 }
