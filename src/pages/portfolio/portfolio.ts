288081c2294430170b71870749e1f643b8df4c60
ayrtoncarlos <bitao.metal@gmail.com>
2018-10-02 02:41:06 -0300

    adicionando metodos para captura dos dados do portfolio.

--- src/pages/portfolio/portfolio.ts
+++ src/pages/portfolio/portfolio.ts
@@ -14,6 +14,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PortfolioPage {
 
+  titles: string[];
+  descriptions: string[];
   images = ['portf1.jpg', 'portf2.jpg', 'portf3.png'];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
@@ -21,6 +23,83 @@ export class PortfolioPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PortfolioPage');
+  }
+
+  updatePortfolio(portfolioName, title, description, newPortfolioName){
+
+    var index = -1;
+
+    if(this.existPortfolio(portfolioName))
+    {
+      index = this.images.indexOf(portfolioName);
+    }
+    if(index != -1)
+    {
+      this.titles[index] = title;
+      this.descriptions[index] = description;
+      this.images[index] = newPortfolioName;
+
+      console.log("Portfolio atualizado com sucesso.");
+    }
+    else
+    {
+      console.log("Portfolio não encontrado ou inexistente.");
+    }
+  }
+
+  addPortfolio(title, description, imageName)
+  {
+    if(!this.existPortfolio(title))
+    {
+      this.images.push(imageName);
+      this.titles.push(title);
+      this.descriptions.push(description);
+
+      console.log("Portfolio adicionado com sucesso.");
+    }
+    else
+    {
+      console.log("Portfolio já existente.");
+    }
+  }
+
+  existPortfolio(title)
+  {
+    return (title in this.titles);
+  }
+
+  deletePortfolio(title)
+  {
+    if(this.isEmpty())
+    {
+      if(title in this.titles)
+      {
+        var index = this.titles.indexOf(title);
+
+        delete this.titles[index];
+        delete this.descriptions[index];
+        delete this.images[index];
+
+        console.log("Portfolio removido com sucesso.");
+      }
+      else
+      {
+        console.log("Portfolio não encontrado ou inexistente.");
+      }
+    }
+    else if(!this.isEmpty())
+    {
+      console.log("Não existe nenhum Portfolio cadastrado no sistema.");
+    }
+    else
+    {
+      console.log("Portfolio não encontrado ou inexistente.");
+    }
+  }
+
+  isEmpty()
+  {
+    return (this.titles.length > 0);
   }
 
 }
