33f63cb80658eeb431cb2231ae177184a4b7ab04
ayrtoncarlos <bitao.metal@gmail.com>
2018-10-03 00:23:16 -0300

    consertando metodos da classe portfolio-admin e portfolio

--- src/pages/portfolio/portfolio.ts
+++ src/pages/portfolio/portfolio.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the PortfolioPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { PortfolioAdminPage } from './portfolio-admin/portfolio-admin';
 
 @Component({
   selector: 'page-portfolio',
@@ -14,92 +8,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PortfolioPage {
 
-  titles: string[];
-  descriptions: string[];
-  images = ['portf1.jpg', 'portf2.jpg', 'portf3.png'];
+  admin: PortfolioAdminPage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PortfolioPage');
-  }
-
-  updatePortfolio(portfolioName, title, description, newPortfolioName){
-
-    var index = -1;
-
-    if(this.existPortfolio(portfolioName))
-    {
-      index = this.images.indexOf(portfolioName);
-    }
-    if(index != -1)
-    {
-      this.titles[index] = title;
-      this.descriptions[index] = description;
-      this.images[index] = newPortfolioName;
-
-      console.log("Portfolio atualizado com sucesso.");
-    }
-    else
-    {
-      console.log("Portfolio não encontrado ou inexistente.");
-    }
-  }
-
-  addPortfolio(title, description, imageName)
+  getPortfolio(index)
   {
-    if(!this.existPortfolio(title))
+    if(!this.isEmpty())
     {
-      this.images.push(imageName);
-      this.titles.push(title);
-      this.descriptions.push(description);
-
-      console.log("Portfolio adicionado com sucesso.");
-    }
-    else
-    {
-      console.log("Portfolio já existente.");
-    }
-  }
-
-  existPortfolio(title)
-  {
-    return (title in this.titles);
-  }
-
-  deletePortfolio(title)
-  {
-    if(this.isEmpty())
-    {
-      if(title in this.titles)
+      if(this.admin.titles.length >= index)
       {
-        var index = this.titles.indexOf(title);
-
-        delete this.titles[index];
-        delete this.descriptions[index];
-        delete this.images[index];
-
-        console.log("Portfolio removido com sucesso.");
+        console.log(this.admin.images[index]);
+        console.log(this.admin.titles[index]);
+        console.log(this.admin.descriptions[index]);
       }
       else
       {
-        console.log("Portfolio não encontrado ou inexistente.");
+        console.log("Índice inválido.");
       }
-    }
-    else if(!this.isEmpty())
-    {
-      console.log("Não existe nenhum Portfolio cadastrado no sistema.");
     }
     else
     {
-      console.log("Portfolio não encontrado ou inexistente.");
+      console.log("Não existe nenhum Portfolio cadastrado no sistema.");
     }
   }
 
-  isEmpty()
+  getPortfolios()
   {
-    return (this.titles.length > 0);
+    if(!this.isEmpty())
+    {
+      for(var i = 0; i < this.admin.titles.length; i++)
+      {
+        console.log(this.admin.images[i]);
+        console.log(this.admin.titles[i]);
+        console.log(this.admin.descriptions[i]);
+      }
+    }
+    else
+    {
+      console.log("Não existe nenhum Portfolio cadastrado no sistema.");
+    }
+  }
+
+  private isEmpty()
+  {
+    return (this.admin.titles.length > 0);
   }
 
 }
